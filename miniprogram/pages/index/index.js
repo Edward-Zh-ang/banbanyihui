2f57194c36c9b967a1fc52aa8726383ec10cdc55
张恺祺 <328787937@qq.com>
2020-01-20 22:23:25 +0800

    design

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,23 +1,80 @@
 // pages/index/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    longitude: [],
+    latitude: [],
+    markers: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+ /**
+ * 生命周期函数--监听页面加载
+ */
   onLoad: function (options) {
 
   },
 
+
+  getBlessing: function () {
+    var that = this;
+    var getUserPic = function (pic_url, i) {
+      let cachePath;
+      if (pic_url == null || pic_url == '') return;
+      wx.downloadFile({
+        url: pic_url,
+        success: (pathInfo) => {
+          // pathInfo.path 这是下载成的缓存链接，模拟器marker有时不支持http开头，真机不影响，得去掉http:/
+          cachePath = pathInfo.tempFilePath.replace("http:/", '').replace("https:/", '')//真机中无需replace，都支持，
+          var mak = "markers[" + i + "].iconPath";
+          that.setData({
+            [mak]: cachePath
+          })
+        }
+      })
+
+    }
+
+    wx.request({
+      url: '你的后台请求地址',
+      method: 'POST',
+      success: function (res) {
+        var list = res.data.list;
+        var tempArr = [];
+        var includeArr = [];
+        var item = {};
+        var includeItem = {};
+        for (var i = 0; i < list.length; i++) {
+          var pic = getUserPic(list[i].user.user_pic, i);
+          item = {
+            iconPath: '/images/icon/small-logo.png',
+            id: list[i].id,
+            latitude: list[i].latitude,
+            longitude: list[i].longitude,
+            width: 30,
+            height: 30,
+            alpha: 0.8,
+          }
+
+          includeItem = {
+            latitude: list[i].latitude,
+            longitude: list[i].longitude,
+          }
+          tempArr.push(item);
+          includeArr.push(includeItem);
+        }
+        that.setData({
+          markers: tempArr,
+          includepoints: includeArr,
+          count: res.data.count
+        })
+        console.log(that.data.markers)
+      }
+    })
+  },
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+     * 生命周期函数--监听页面初次渲染完成
+     */
   onReady: function () {
 
   },
@@ -47,13 +104,6 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
   },
 
---
+++ miniprogram/pages/updatelocation/updatelocation.js
@@ -0,0 +1,181 @@
+const db = wx.cloud.database()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    image: null,
+    longitude: [],
+    latitude: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                image: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+          // console.log('authSetting:status:拒绝授权后再次进入重新授权', res.authSetting['scope.userLocation'])
+          wx.showModal({
+            title: '',
+            content: '班班易会需要获取你的地理位置，请确认授权',
+            success: function (res) {
+              if (res.cancel) {
+                wx.showToast({
+                  title: '拒绝授权',
+                  icon: 'none'
+                })
+                setTimeout(() => {
+                  wx.navigateBack()
+                }, 1500)
+              } else if (res.confirm) {
+                wx.openSetting({
+                  success: function (dataAu) {
+                    // console.log('dataAu:success', dataAu)
+                    if (dataAu.authSetting["scope.userLocation"] == true) {
+                      //再次授权，调用wx.getLocation的API
+                      vm.getLocation(dataAu)
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'none'
+                      })
+                      setTimeout(() => {
+                        wx.navigateBack()
+                      }, 1500)
+                    }
+                  }
+                })
+              }
+            }
+          })
+        }
+        // 初始化进入，未授权
+        else if (res.authSetting['scope.userLocation'] == undefined) {
+          // console.log('authSetting:status:初始化进入，未授权', res.authSetting['scope.userLocation'])
+          //调用wx.getLocation的API
+          vm.getLocation(res)
+        }
+        // 已授权
+        else if (res.authSetting['scope.userLocation']) {
+          // console.log('authSetting:status:已授权', res.authSetting['scope.userLocation'])
+          //调用wx.getLocation的API
+          vm.getLocation(res)
+        }
+      }
+    })
+  },
+  // 微信获得经纬度
+  getLocation: function (userLocation) {
+    let vm = this
+    wx.getLocation({
+      type: "wgs84",
+      success: function (res) {
+        // console.log('getLocation:success', res)
+        var latitude = res.latitude
+        var longitude = res.longitude
+        vm.getDaiShu(latitude, longitude)
+      },
+      fail: function (res) {
+        // console.log('getLocation:fail', res)
+        if (res.errMsg === 'getLocation:fail:auth denied') {
+          wx.showToast({
+            title: '拒绝授权',
+            icon: 'none'
+          })
+          setTimeout(() => {
+            wx.navigateBack()
+          }, 1500)
+          return
+        }
+        if (!userLocation || !userLocation.authSetting['scope.userLocation']) {
+          vm.getUserLocation()
+        } else if (userLocation.authSetting['scope.userLocation']) {
+          wx.showModal({
+            title: '',
+            content: '请在系统设置中打开定位服务',
+            showCancel: false,
+            success: result => {
+              if (result.confirm) {
+                wx.navigateBack()
+              }
+            }
+          })
+        } else {
+          wx.showToast({
+            title: '授权失败',
+            icon: 'none'
+          })
+          setTimeout(() => {
+            wx.navigateBack()
+          }, 1500)
+        }
+      }
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
